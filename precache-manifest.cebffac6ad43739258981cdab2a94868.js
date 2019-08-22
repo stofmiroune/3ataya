b7ed12b9b94e2854707529236047eb6c4e003235
JsnS <jsns@Aeorx-V01.local>
2019-08-22 20:10:20 +0100

    done

---
+++ precache-manifest.cebffac6ad43739258981cdab2a94868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0402a3a4b72184f915d0b8cb07046e3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00fde845e344dad5fb56",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "288ea01a59173be902a7",
+    "url": "/static/css/main.309c4cf6.chunk.css"
+  },
+  {
+    "revision": "00fde845e344dad5fb56",
+    "url": "/static/js/2.6fb0d3a3.chunk.js"
+  },
+  {
+    "revision": "288ea01a59173be902a7",
+    "url": "/static/js/main.f758f40c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
